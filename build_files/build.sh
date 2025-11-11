0d99aa6f3e94533f1006dd12715172c8c7b286e4
inkunome <ink.dev@outlook.fr>
2025-11-11 08:00:18 +0100

    do not clear cache

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,31 +4,9 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-
-dnf5 group remove 'GNOME Desktop Environment'
 dnf5 install -y @cosmic-desktop-environment
-
-# Clear cache
-dnf5 clean all 
-rm -rf /var/cache/dnf/*
 
 # Switch to cosmic greeter
 systemctl disable display-manager 
 systemctl enable cosmic-greeter.service -f
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-# systemctl enable podman.socket
