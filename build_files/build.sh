5da89be9bae48391e138cfbf700e3646e24028b8
inkunome <ink.dev@outlook.fr>
2025-11-11 07:34:13 +0100

    adding-cosmic

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,16 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+
+dnf5 install -y @cosmic-desktop-environment
+
+# Clear cache
+dnf5 clean all 
+rm -rf /var/cache/dnf/*
+
+# Switch to cosmic greeter
+systemctl disable display-manager 
+systemctl enable cosmic-greeter.service -f
 
 # Use a COPR Example:
 #
