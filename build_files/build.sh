412d9880b02e60b0c1303afcc8ef1c3f58c0369c
inkunome <ink.dev@outlook.fr>
2025-11-11 08:05:41 +0100

    do not clear cache

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,11 @@ set -ouex pipefail
 ### Install packages
 
 dnf5 install -y @cosmic-desktop-environment
+dnf5 remove -y libreoffice
+
+# Clear cache
+dnf5 clean all 
+rm -rf /var/cache/dnf/*
 
 # Switch to cosmic greeter
 systemctl disable display-manager 
