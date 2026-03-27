9d023dd726ae99a2e8d90034ef9d4d54b311c00c
Inkunome <dev.ink@outlook.fr>
2026-03-27 14:17:39 +0100

    adding nemo

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 
 dnf5 copr enable -y swayfx/swayfx
 dnf5 copr enable -y varlad/zellij
-dnf5 install -y swayfx kanshi alacritty wofi fira-code-fonts zellij waybar blueman pavucontrol
+dnf5 install -y swayfx kanshi alacritty wofi fira-code-fonts zellij waybar blueman pavucontrol nemo
 
 # curl -fsSL https://install.determinate.systems/nix | sh -s -- install ostree --no-confirm
 
